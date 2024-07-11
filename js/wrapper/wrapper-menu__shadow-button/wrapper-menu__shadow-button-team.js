160f025b65acecc3d90d8c9d0cf0d2190385141d
lolgh57 <sasha.over.game@gmail.com>
2024-07-11 02:29:23 +0200

    edits

--- js/wrapper/wrapper-menu__shadow-button/wrapper-menu__shadow-button-team.js
+++ js/wrapper/wrapper-menu__shadow-button/wrapper-menu__shadow-button-team.js
@@ -25,37 +25,48 @@ buttonTeam.addEventListener("mouseleave", function () {
   teamElem2.classList.remove("team__elem2_push_down");
 });
 
-buttonTeam.addEventListener("click", function () {
-  setTimeout(() => (window.location.href = "team.html"), 500);
-});
-buttonTeam.addEventListener("click", function () {
-  wrapperMenu.classList.add("wrapper-menu_full-opacity");
-  setTimeout(hideLeftRightElemtnsFirst, 500);
-  function hideLeftRightElemtnsFirst() {
-    buttonMainPage.classList.add("zero-opacity");
-    buttonSiteMap.classList.add("zero-opacity");
-    setTimeout(hideLeftRightElemtnsSecond, 500);
-    function hideLeftRightElemtnsSecond() {
-      buttonPartners.classList.add("zero-opacity");
-      buttonProducts.classList.add("zero-opacity");
-      setTimeout(hideLeftRightElemtnsThirt, 500);
-      function hideLeftRightElemtnsThirt() {
-        buttonTeam.classList.add("zero-opacity");
-        wrapperMenu.classList.add("zero-opacity_long");
-        setTimeout(wrapperMenuTeamHide, 1000);
-        window.location.href = "team.html";
-        function wrapperMenuTeamHide() {
-          wrapperMenu.classList.add("hide-element");
-          setTimeout(teamMenuShow, 5000);
-          function teamMenuShow() {
-            wrapperTeam.classList.remove("hide-element");
-            setTimeout(teamMenuShowOpacityRemove, 500);
-            function teamMenuShowOpacityRemove() {
-              wrapperTeam.classList.remove("zero-opacity");
+// buttonTeam.addEventListener("click", function () {
+//   setTimeout(() => (window.location.href = "team.html"), 500);
+// });
+
+function isIphone() {
+  return /iPhone|iPad|iPod/i.test(navigator.userAgent);
+}
+
+if (isIphone()) {
+  buttonTeam.addEventListener("click", function () {
+    setTimeout(() => (window.location.href = "team.html"), 500);
+  });
+} else {
+  buttonTeam.addEventListener("click", function () {
+    wrapperMenu.classList.add("wrapper-menu_full-opacity");
+    setTimeout(hideLeftRightElemtnsFirst, 500);
+    function hideLeftRightElemtnsFirst() {
+      buttonMainPage.classList.add("zero-opacity");
+      buttonSiteMap.classList.add("zero-opacity");
+      setTimeout(hideLeftRightElemtnsSecond, 500);
+      function hideLeftRightElemtnsSecond() {
+        buttonPartners.classList.add("zero-opacity");
+        buttonProducts.classList.add("zero-opacity");
+        setTimeout(hideLeftRightElemtnsThirt, 500);
+        function hideLeftRightElemtnsThirt() {
+          buttonTeam.classList.add("zero-opacity");
+          wrapperMenu.classList.add("zero-opacity_long");
+          setTimeout(wrapperMenuTeamHide, 1000);
+          window.location.href = "team.html";
+          function wrapperMenuTeamHide() {
+            wrapperMenu.classList.add("hide-element");
+            setTimeout(teamMenuShow, 5000);
+            function teamMenuShow() {
+              wrapperTeam.classList.remove("hide-element");
+              setTimeout(teamMenuShowOpacityRemove, 500);
+              function teamMenuShowOpacityRemove() {
+                wrapperTeam.classList.remove("zero-opacity");
+              }
             }
           }
         }
       }
     }
-  }
-});
+  });
+}
